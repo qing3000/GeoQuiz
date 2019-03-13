048fd7bd103df37f145041fa88905ef28c0cac90
zhangq <qing3000@gmail.com>
2019-03-13 21:07:45 +0000

    Cheat activity added.

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -10,11 +12,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     private Button mTrueButton;
     private Button mFalseButton;
-
     private ImageButton mNextButton;
     private ImageButton mPreviousButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
     private Question[] mQuestionBank = new Question[]
     {
@@ -32,7 +37,14 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null)
+        {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            Log.i(TAG, String.format("currentIndex=%d", mCurrentIndex));
+        }
 
         // Set up question.
         mQuestionTextView = findViewById(R.id.question_text_view);
@@ -83,11 +95,60 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        updateQuestion(1);
+        mCheatButton = findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(MainActivity.this, answerIsTrue);
+                startActivity(intent);
+            }
+        });
+        updateQuestion(0);
+    }
+
+    @Override public void onStart()
+    {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override public void onResume()
+    {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override public void onPause()
+    {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override public void onSaveInstanceState(Bundle savedInstanceState)
+    {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, String.format("onSaveInstanceState(), currentIndex=%d", mCurrentIndex));
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override public void onStop()
+    {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override public void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     private void updateQuestion(int step)
     {
+        Log.i(TAG, String.format("OldIndex=%d", mCurrentIndex));
         mCurrentIndex = mCurrentIndex + step;
         if (mCurrentIndex < 0)
         {
@@ -97,6 +158,8 @@ public class MainActivity extends AppCompatActivity
         {
             mCurrentIndex = mCurrentIndex % mQuestionBank.length;
         }
+
+        Log.i(TAG, String.format("NewIndex=%d", mCurrentIndex));
 
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
